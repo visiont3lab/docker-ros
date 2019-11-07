d91f4e38a4411726eee51f567f10560ed6770479
manuelrucci <rucci@localhost.com>
2019-11-07 15:01:53 +0100

    updated laser sim

--- scripts/launch_laser_sim.sh
+++ scripts/launch_laser_sim.sh
@@ -8,17 +8,17 @@ exec bash\""  \
 sleep 4 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'cd /root/catkin_ws && source /opt/ros/melodic/setup.bash && catkin_make && source /root/catkin_ws/devel/setup.bash && roscore';
 exec bash\""  \
 --tab --title "LASER PUBLISHER" --command "bash -c \"
-sleep 30 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_publisher laser_publisher.launch --wait';
+sleep 20 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_publisher laser_publisher.launch --wait';
 exec bash\""  \
 --tab --title "TF"	--command "bash -c \"
-sleep 30 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch tf_publisher tf_publisher.launch  --wait';
+sleep 20 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch tf_publisher tf_publisher.launch  --wait';
 exec bash\""  \
 --tab --title "LASER ASSEMBLER"	--command "bash -c \"
-sleep 30 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_assembler_demo assembler.launch --wait';
+sleep 20 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_assembler_demo assembler.launch --wait';
 exec bash\""  \
 --tab --title "ASSEMBLER SERVICE START"	--command "bash -c \"
-sleep 30 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_assembler_demo service_client.launch --wait';
+sleep 20 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && roslaunch laser_assembler_demo service_client.launch --wait';
 exec bash\"" \
 --tab --title "RVIZ"	--command "bash -c \"
-sleep 45 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && rosrun rviz rviz -d /root/catkin_ws/src/configs/rviz_sim.rviz ';
+sleep 35 && docker exec -it ros_melodic_desktop_sim /bin/bash -c 'source /root/catkin_ws/devel/setup.bash && rosrun rviz rviz -d /root/catkin_ws/src/configs/rviz_sim.rviz ';
 exec bash\"" & 
